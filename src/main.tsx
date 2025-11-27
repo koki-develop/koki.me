a3ee215ee4fcfdcd8efc541626590335f03b4742
koki-develop <kou.pg.0131@gmail.com>
2025-11-27 20:07:25 +0900

    refactor: migrate routing to use createBrowserRouter and RouterProvider
    - Replaced BrowserRouter and Routes with new router API
    - Defined routes as objects with Component properties
    - Simplifies routing setup and improves code clarity

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./Layout";
 import AboutPage from "./pages/AboutPage";
 import NotesPage from "./pages/NotesPage";
@@ -9,17 +9,32 @@ import WorksPage from "./pages/WorksPage";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: AboutPage,
+      },
+      {
+        path: "works",
+        Component: WorksPage,
+      },
+      {
+        path: "notes",
+        Component: NotesPage,
+      },
+      {
+        path: "*",
+        Component: NotFoundPage,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<AboutPage />} />
-          <Route path="works" element={<WorksPage />} />
-          <Route path="notes" element={<NotesPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
