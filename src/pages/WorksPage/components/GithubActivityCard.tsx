586203b683f4479f530a6c189d5b3b2203ccdf46
koki-develop <kou.pg.0131@gmail.com>
2026-07-25 08:49:02 +0900

    refactor: remove Card wrapper from GithubActivityCard

--- src/pages/WorksPage/components/GithubActivityCard.tsx
+++ src/pages/WorksPage/components/GithubActivityCard.tsx
@@ -1,5 +1,5 @@
 import type { Contributions } from "@/types";
-import { Card, ContributionGraph, Stack, Text } from "@ps1ui/core";
+import { ContributionGraph, Stack, Text } from "@ps1ui/core";
 
 type GithubActivityCardProps = {
   contributions: Contributions;
@@ -7,19 +7,14 @@ type GithubActivityCardProps = {
 
 export function GithubActivityCard({ contributions }: GithubActivityCardProps) {
   return (
-    <Card>
-      <Stack gap="lg">
-        <Stack direction="row" align="baseline" wrap gap="md">
-          <Text as="span" size="sm" weight="bold">
-            GitHub activity
-          </Text>
-          <Text as="span" variant="subtle" size="xs">
-            {contributions.total.toLocaleString("en-US")} contributions in the
-            last year
-          </Text>
-        </Stack>
-        <ContributionGraph data={contributions.days} />
+    <Stack gap="lg">
+      <Stack direction="row" align="baseline" wrap gap="md">
+        <Text as="span" variant="subtle" size="xs">
+          {contributions.total.toLocaleString("en-US")} contributions in the
+          last year
+        </Text>
       </Stack>
-    </Card>
+      <ContributionGraph data={contributions.days} />
+    </Stack>
   );
 }
